31748263ba4107b089219d52872d7bafa53e40f5
calebji123 <30414755+calebji123@users.noreply.github.com>
2018-11-05 14:11:41 -0700

    Update index.js

--- scienceproject/index.js
+++ scienceproject/index.js
@@ -1,33 +1,13 @@
-var slideAmount = 3
+
+
 var slide1 = document.getElementById("slide1")
 var slide2 = document.getElementById("slide2")
 var slide3 = document.getElementById("slide3")
-slide1.style.display = "block"
-var slideNum = 1
-document.getElementById("slideButtonForward").onclick = function(){
-  if (slideNum == 1) {
-    slide1.style.display = "none"
-    slide2.style.display = "block"
-  } else if (slideNum == 2) {
-    slide3.style.display = "block"
-    slide2.style.display = "none"
-  }
-  if (slideNum < slideAmount) {
-      slideNum = slideNum + 1
-  }
-}
-document.getElementById("slideButtonBack").onclick = function(){
-  if (slideNum == 2) {
-    slide1.style.display = "block"
-    slide2.style.display = "none"
-  } else if (slideNum == 3) {
-    slide3.style.display = "none"
-    slide2.style.display = "block"
-  }
-  if (slideNum >= 2) {
-      slideNum = slideNum - 1
-  }
-}
+var slide4 = document.getElementById("slide4")
+slide1.style.display = "none"
+slide2.style.display = "none"
+slide3.style.display = "none"
+slide4.style.display = "none"
 
 
 document.getElementById("mainMain").onclick = function(){
